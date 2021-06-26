63e45c2e52db697b771dda8daba33cac5d259d9a
dev7k <dev7k@protonmail.com>
2021-06-26 23:46:38 +0200

    Added project benchmarks

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using BenchmarkDotNet.Running;
 
 namespace JsonBenchmarks
 {
@@ -6,7 +6,7 @@ namespace JsonBenchmarks
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BenchmarkRunner.Run<JsonBenchmarks>();
         }
     }
 }
---
+++ User.cs
@@ -0,0 +1,28 @@
+namespace JsonBenchmarks
+{
+    public record User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Website { get; set; }
+        public string Phone { get; set; }
+        public int Age { get; set; }
+        public int Posts { get; set; }
+        public string Status { get; set; }
+        public string Profile { get; set; }
+        public string Address { get; set; }
+        public string Country { get; set; }
+        public bool? Active { get; set; }
+        public string Password0 { get; set; }
+        public string Password1 { get; set; }
+        public string Password2 { get; set; }
+        public string Password3 { get; set; }
+        public string Password4 { get; set; }
+        public string Password5 { get; set; }
+        public string Password6 { get; set; }
+        public string Password7 { get; set; }
+        public string Password8 { get; set; }
+        public string Password9 { get; set; }
+   }
+}
